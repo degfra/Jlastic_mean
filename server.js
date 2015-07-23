b4502b85fde05364ebc1790b3347af175ddddae0
degfra <f.degrandi@stemcode.ch>
2015-07-23 13:30:45 +0200

    Updated server.js with file I/O

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 /**
-<<<<<<< HEAD
  * Created by francois on 7/22/15.
-=======
- * Created by francois de grandi on 7/21/15.
->>>>>>> serverjs
  */
 
 var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
+    if (req.url === '/favicon.ico') {
+        return res.end();
+    }
+    console.log('Incoming request to ' + req.url);
+    var i = 2;
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    setTimeout(function() {
+        fs.readFile(__filename, {
+            encoding: 'utf8'
+        }, function (error, contents) {
+            if (error) {
+                console.error(error);
+                return res.end();
+            }
+            console.log('sending response for ' + req.url);
+            res.end(contents);
+        });
+    }, 5000);
+    while(i--) {
+        console.log('Loop value: '+ i + '\r');
+    }
 }).listen(3000, '127.0.0.1');
-<<<<<<< HEAD
+
 console.log('Server running at http://127.0.0.1:3000/');
-=======
-console.log('Server running at http://127.0.0.1:3000/');
->>>>>>> serverjs
+
+
+
