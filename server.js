30824767f7579373cb11a4d53dd2ac32f4a59871
degfra <f.degrandi@stemcode.ch>
2015-07-24 18:00:14 +0200

    Port 1337

--- server.js
+++ server.js
@@ -5,9 +5,9 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Mean Stack\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
 
 
 
